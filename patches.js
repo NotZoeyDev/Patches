438ed19c8049e82a0abac003f2f83b98e5c5707d
Zoey <zoey@panties.moe>
2021-05-12 17:18:48 -0400

    Script now imports author and description from tweak itself, also will package js files if needed.

--- patches.js
+++ patches.js
@@ -6,9 +6,9 @@ const template = `Package: package_name
 Name: patch_name
 Version: patch_version
 Architecture: iphoneos-arm
-Description: A patch for DiscordExtras.
-Maintainer: Zoey
-Author: Zoey
+Description: patch_description
+Maintainer: patch_author
+Author: patch_author
 Section: Patches
 Depends: moe.panties.discordextras
 `;
@@ -27,6 +27,8 @@ const patches = fs.readdirSync("patches");
 const packages = fs.readdirSync("packages");
 
 for (const patch of patches) {
+  if (!patch.includes(".json")) continue;
+  const patchInfo = JSON.parse(fs.readFileSync(`patches/${patch}`, { encoding: 'utf-8' }));
   const patchName = patch.replace('.json', '').toLowerCase();
   const tempFolder = `temp/${patchName}`;
   
@@ -40,10 +42,17 @@ for (const patch of patches) {
   const controlFile = template
     .replace('package_name', packageName)
     .replace('patch_name', patch.replace('.json', ''))
+    .replace('patch_description', patchInfo.description)
+    .replaceAll('patch_author', patchInfo.author)
     .replace('patch_version', `1.0.0-${version + 1}`);
 
   fs.writeFileSync(`${tempFolder}/DEBIAN/control`, controlFile);
   fs.copyFileSync(`patches/${patch}`, `${tempFolder}/Library/Application Support/DiscordExtrasFiles.bundle/patches/${patch}`);
+
+  const jsFile = patch.replace('.json', '.js');
+  if (fs.existsSync(`patches/${jsFile}`)) {
+    fs.copyFileSync(`patches/${jsFile}`, `${tempFolder}/Library/Application Support/DiscordExtrasFiles.bundle/patches/${jsFile}`);
+  }
 
   execSync(`dpkg-deb --build ${tempFolder}`);
   fs.renameSync(`temp/${patchName}.deb`, `packages/${patchName}_1.0.0-${version + 1}_iphoneos-arm.deb`)
