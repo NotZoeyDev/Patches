95f06bc439e9948d33b4ed062ee85b62a9f6f560
Zoey <zoey@panties.moe>
2021-05-12 17:19:44 -0400

    CallConfirmation patch, made by @aspenluxxxy

---
+++ patches/CallConfirmation.js
@@ -0,0 +1,2 @@
+var channelName=(0,modCC.computeChannelName)(n,A.default,M.default);
+C.default.show({title: "Call?", body: `Are you sure you want to start a call with ${channelName}?`,confirmText:"Yes, start the call",cancelText:"Nope, that was an accident",onConfirm:function(){o.default.call(n.id,l,u&&!n.isManaged(),s,function(){return T(n)})},onCancel: N.NOOP})
